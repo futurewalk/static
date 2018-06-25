34465d43c5a2ccd3eff29bff8b7aecd27536f548
847566734@qq.com <847566734@qq.com>
2018-06-25 11:41:36 +0800

    update doc-cool ui

--- extend/extend.js
+++ extend/extend.js
@@ -8,12 +8,33 @@ var Resolver = {}
 // in this method ,you can replace this data to another protoBuf message for field type bytes
 Resolver.byteInvoke = function (plugin) {
 
+    if (plugin.StructureName == "CoolRequest" && plugin.FieldName == "data") {
+        plugin.Swap("DataName");
+    }
+    if (plugin.StructureName == "CoolRequest" && plugin.FieldName == "Optype") {
+        //plugin.Remove()
+    }
+    if (plugin.StructureName == "Request" && plugin.FieldName == "data" && plugin.Url == "/v1/device/login") {
+        plugin.Swap("LoginReqData")
+    }
+    if (plugin.StructureName == "Request" && plugin.FieldName == "data" && plugin.Url == "/v1/device/device-bind-status") {
+        plugin.Swap("QueryDeviceBindStatusReqData")
+    }
+    if (plugin.StructureName == "Request" && plugin.FieldName == "data" && plugin.Url == "/v1/device/get-deviceInfo") {
+        plugin.Swap("GetDeviceInfoReqData","userBind.proto");
+    }
+    if (plugin.StructureName == "BytesMessageReq" && plugin.FieldName == "Data" && plugin.Url == '/v1/example/BytesSupport') {
+        plugin.Swap("BytesMessageInfo");
+    }
+
 }
 // in this callBack method ,you can set a field with a custom data type on another protobuf file。
 Resolver.dataInvoke = function (plugin){
 
 }
 Resolver.RspInvoke = function (plugin) {
-
+    if (plugin.StructureName == "BytesMessageRsp" && plugin.FieldName == "Data" && plugin.Url == '/v1/example/BytesSupport') {
+        plugin.Swap("BytesMessageInfo");
+    }
 }
 window.Resolver = Resolver;
